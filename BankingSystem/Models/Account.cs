45f10034201e6fbf6e37c5b57155ceb29e918948
pas-mayank-dewan <70316437+pas-mayank-dewan@users.noreply.github.com>
2024-07-01 23:34:21 +0530

    reverting changes

--- BankingSystem/Models/Account.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BankingSystem.Models
-{
-    public class Account
-    {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public decimal Balance { get; set; }
-    }
-}
