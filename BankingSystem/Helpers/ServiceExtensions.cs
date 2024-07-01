91cc7c7bccbd4761a5ba5e1a7cef20550087d671
pas-mayank-dewan <70316437+pas-mayank-dewan@users.noreply.github.com>
2024-07-01 22:26:37 +0530

    Adding Project

---
+++ BankingSystem/Helpers/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using BankingSystem.Contracts;
+using BankingSystem.Services;
+
+namespace BankingSystem.Helpers
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection RegisterAppServices(this IServiceCollection services)
+        {
+            services.AddSingleton<InMemoryDataStore>();
+            services.AddScoped<IUserService,UserService>();
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddControllers();
+            return services;
+        }
+    }
+}
